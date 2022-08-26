877474261798570085d4ca89e5cf8a166ac964f7
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-26 15:33:14 +0800

    Add files via upload

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -6,11 +6,10 @@ extern int* extract_negatives(int *vals,int len, int num_negative);
 
 int main()
 {
-    int arr[]={-5,-3,-1,0,1,2,3,-6};
+    int arr[]={-2,-7,5,6,-8};
 
-    int len=8;
-    cout<<"number of negative integers in the given array are(calling count_negative first time) :" <<negative_count(arr,len)<<endl;
-    cout<<"number of negative integers in the given array are(calling count_negative second time) :" <<negative_count(arr,len)<<endl;
+    int len=5;
+
 
     int num_negative=negative_count(arr,len);
     int *a = extract_negatives(arr,len,num_negative);
