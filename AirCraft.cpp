499a7f57bec8e2b0caa02cf659c208ed7900e235
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-10-28 14:29:11 +0800

    Add files via upload

---
+++ AirCraft.cpp
@@ -0,0 +1,19 @@
+#include 'AirCraft.h'
+
+AirCraft::AirCraft(){}
+
+
+void AirCraft::refuel()
+{
+    this -> refuel = 100;
+}
+void AirCraft::set_fuel(float f){
+    this -> f = 0;
+}
+
+float AirCraft:: get_feul(){
+    this -> feul;
+}
+
+void AirCraft::fly(int headwind, int minutes)
+
